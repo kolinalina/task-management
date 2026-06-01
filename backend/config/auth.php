d96db4c2f1eb1f22dd1ce516333231c546ba23b7
kolinalina <zanikolina@gmail.com>
2026-06-01 23:07:15 +0700

    updating auth

--- backend/config/auth.php
+++ backend/config/auth.php
@@ -15,9 +15,13 @@ return [
     |
     */
 
+    // 'defaults' => [
+    //     'guard' => env('AUTH_GUARD', 'web'),
+    //     'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+    // ],
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+        'guard' => 'api',
+        'passwords' => 'users',
     ],
 
     /*
@@ -38,10 +42,11 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
+        // 'web' => [
+        //     'driver' => 'session',
+        //     'provider' => 'users',
+        // ],
+        'api' => ['driver' => 'jwt', 'provider' => 'users'],
     ],
 
     /*
